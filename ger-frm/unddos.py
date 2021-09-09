5d6c85919d273a02a6043cca991eb06e63d53502
nachorivas581 <64506488+nachorivas581@users.noreply.github.com>
2021-09-09 21:14:26 +0200

    Update unddos.py

--- ger-frm/unddos.py
+++ ger-frm/unddos.py
@@ -1,9 +1,6 @@
 #!/bin/bash
-Block="/etc/bin" && [[ ! -d ${Block} ]] && exit
-Block > /dev/null 2>&1
-clear
 cowsay -f eyes "esta herramienta desinstala la version pasada de ANTI-DDoS" | lolcat 
-figlet ..dankelthaher.. | lolcat
+figlet ..Nexuser.. | lolcat
 BARRA1="\e[0;31m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\e[0m"
 echo -e "$BARRA1"
 
